bb7fac0f1d926c104b9dbbf23bd3f5458833611e
Mateusz Zagórski <mateuszzagorski1@gmail.com>
2023-02-21 12:55:50 +0100

    Overview - components and stuff added.

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Products from "scenes/products";
 import Customers from "scenes/customers";
 import Transactions from "scenes/transactions";
 import Geography from "scenes/geography";
+import Overview from "scenes/overview";
 
 function App() {
 	const mode = useSelector((state) => state.global.mode);
@@ -33,6 +34,7 @@ function App() {
 								element={<Transactions />}
 							/>
 							<Route path="/geography" element={<Geography />} />
+							<Route path="/overview" element={<Overview />} />
 						</Route>
 					</Routes>
 				</ThemeProvider>
