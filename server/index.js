d65c67547f0349dad14c92e1c11624f8cd80ac1f
Mateusz Zagórski <mateuszzagorski1@gmail.com>
2023-02-21 12:55:22 +0100

    Overview chart server-side added.

--- server/index.js
+++ server/index.js
@@ -15,14 +15,15 @@ import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
-// import OverallStat from "./models/OverallStat.js";
+import OverallStat from "./models/OverallStat.js";
 // import AffiliateStat from "./models/AffiliateStat.js";
+
 import {
 	dataUser,
 	dataProduct,
 	dataProductStat,
 	dataTransaction,
-	//   dataOverallStat,
+	dataOverallStat,
 	//   dataAffiliateStat,
 } from "./data/index.js";
 
